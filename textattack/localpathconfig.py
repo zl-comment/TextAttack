6a1dee060df4a4337388c1294b1054379e88805d
张磊 <1044836856@qq.com>
2024-11-20 15:32:28 +0800

    修改了代码使得路径更稳健

--- textattack/localpathconfig.py
+++ textattack/localpathconfig.py
@@ -1,9 +1,12 @@
 
+import os
 
 class localpathconfig:
+
+
 #bert-base-uncased
-BERT_BASE_UNCASED = "/home/cyh/ZLCODE/google/bert-base-uncased"
+BERT_BASE_UNCASED =  os.path.abspath(os.path.join(current_dir, '../../google/bert-base-uncased'))
 
-UNIVERSAL_SENTENCE_ENCODER = "/home/cyh/ZLCODE/google/universal-sentence-encoder"
+UNIVERSAL_SENTENCE_ENCODER = os.path.abspath(os.path.join(current_dir, '../../google/universal-sentence-encoder'))
 
-UNIVERSAL_SENTENCE_ENCODER_LARGE = "/home/cyh/ZLCODE/google/universal-sentence-encoder-large"
+UNIVERSAL_SENTENCE_ENCODER_LARGE = os.path.abspath(os.path.join(current_dir, '../../google/universal-sentence-encoder-large'))
