e938bfe7bce7ce1893e0bcbc596f09feed3473f6
张磊 <1044836856@qq.com>
2024-11-22 16:32:34 +0800

    测试

--- textattack/transformations/word_swaps/word_swap_masked_lm.py
+++ textattack/transformations/word_swaps/word_swap_masked_lm.py
@@ -354,7 +354,7 @@ class WordSwapMaskedLM(WordSwap):
             return top_replacements  # 返回替换单词列表
 
     def _get_transformations(self, current_text, indices_to_modify):
-        print("qqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqqq")
+        print(phrases_indices)
         indices_to_modify = list(indices_to_modify)
         if self.method == "bert-attack":
             # 编码当前文本
