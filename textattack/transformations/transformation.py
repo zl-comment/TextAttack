138bb2cdd00e4b22f09c296962ed025af9f98b2a
张磊 <1044836856@qq.com>
2024-11-22 18:36:12 +0800

    修改判断

--- textattack/transformations/transformation.py
+++ textattack/transformations/transformation.py
@@ -97,7 +97,7 @@ class Transformation(ReprMixin, ABC):
         if return_phrases_indices:
             return phrases_indices
 
-        if phrase is not False:
+        if phrase is  False:
             transformed_texts = self._get_transformations(current_text, indices_to_modify)
             for text in transformed_texts:
                 text.attack_attrs["last_transformation"] = self
