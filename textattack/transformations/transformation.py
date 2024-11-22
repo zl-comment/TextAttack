b610be2646e319f22f763b4b14c0dd1dc719c895
张磊 <1044836856@qq.com>
2024-11-22 15:40:58 +0800

    phrases_indices命名修改

--- textattack/transformations/transformation.py
+++ textattack/transformations/transformation.py
@@ -22,7 +22,7 @@ class Transformation(ReprMixin, ABC):
         shifted_idxs=False,
         return_indices=False,
         return_phrases_indices=False,    #是否返回短语索引
-        phrases_indices_to_order=None,  #短语索引
+        phrases_indices=None,  #短语索引
     ):
         """Returns a list of all possible transformations for ``current_text``.
         Applies the ``pre_transformation_constraints`` then calls
@@ -53,39 +53,39 @@ class Transformation(ReprMixin, ABC):
                 current_text.convert_from_original_idxs(indices_to_modify)
             )
         #增加了返回短语 已经是经过筛选的
-        if phrases_indices_to_order is  None:
+        if phrases_indices is  None:
             self.nlp = spacy.load("en_core_web_sm")  # Load the spaCy model
-            phrases_indices_to_order = set()
+            phrases_indices = set()
             # 提取名词短语和单词
             relevant_text = " ".join(current_text.text[i] for i in indices_to_modify)
             doc = self.nlp(relevant_text)
             covered_indices = set()
             for chunk in doc.noun_chunks:
-                phrases_indices_to_order.add((chunk.start, chunk.end, "noun-phrase"))
+                phrases_indices.add((chunk.start, chunk.end, "noun-phrase"))
                 covered_indices.update(range(chunk.start, chunk.end))
 
             # 提取动词短语和固定表达式
             for token in doc:
                 if token.pos_ == "VERB":
-                    phrases_indices_to_order.add((token.i, token.i + 1, "verb-phrase"))
+                    phrases_indices.add((token.i, token.i + 1, "verb-phrase"))
                     covered_indices.add(token.i)
                 elif token.dep_ == "fixed":
-                    phrases_indices_to_order.add((token.i, token.i + 1, "fixed-expression"))
+                    phrases_indices.add((token.i, token.i + 1, "fixed-expression"))
                     covered_indices.add(token.i)
 
             # 添加未覆盖的单词
             for token in doc:
                 if token.i not in covered_indices:
-                    phrases_indices_to_order.add((token.i, token.i + 1, "single-word"))
+                    phrases_indices.add((token.i, token.i + 1, "single-word"))
 
             # 按 token 序号排序
-            phrases_indices_to_order = sorted(phrases_indices_to_order, key=lambda x: x[0])
+            phrases_indices = sorted(phrases_indices, key=lambda x: x[0])
         else:
-            phrases_indices_to_order = set(phrases_indices_to_order)
+            phrases_indices = set(phrases_indices)
 
         for constraint in pre_transformation_constraints:
             indices_to_modify = indices_to_modify & constraint(current_text, self)
-            # phrases_indices_to_order = phrases_indices_to_order & constraint(current_text, self)
+            # phrases_indices = phrases_indices & constraint(current_text, self)
 
         
 
@@ -93,7 +93,7 @@ class Transformation(ReprMixin, ABC):
             return indices_to_modify
         #增加了返回短语的确定
         if return_phrases_indices:
-            return phrases_indices_to_order
+            return phrases_indices
 
         transformed_texts = self._get_transformations(current_text, indices_to_modify)
         for text in transformed_texts:
