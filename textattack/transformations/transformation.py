22f5a5a8b995bf09c958d851a2a7927cb4c42fa6
张磊 <1044836856@qq.com>
2024-11-22 18:12:00 +0800

    缩进

--- textattack/transformations/transformation.py
+++ textattack/transformations/transformation.py
@@ -99,13 +99,13 @@ class Transformation(ReprMixin, ABC):
         if index_to_modify is not None:
             transformed_texts = self._get_transformations(current_text, indices_to_modify)
             for text in transformed_texts:
-            text.attack_attrs["last_transformation"] = self
+                text.attack_attrs["last_transformation"] = self
         return transformed_texts
-        
+
         if phrases_indices is not None:
             transformed_texts_phrases = self._get_transformations_phrases(current_text, phrases_indices)
             for text in transformed_texts_phrases:
-            text.attack_attrs["last_transformation"] = self
+                text.attack_attrs["last_transformation"] = self
         return transformed_texts_phrases    
         
         
