03926a2c266a2842486270a208c05e4be6e549d0
张磊 <1044836856@qq.com>
2024-11-26 11:18:51 +0800

    返回短语

--- textattack/transformations/word_swaps/word_swap_masked_lm_zl_optimized_with_ga.py
+++ textattack/transformations/word_swaps/word_swap_masked_lm_zl_optimized_with_ga.py
@@ -264,12 +264,9 @@ class WordSwapMaskedLM_zl(WordSwap):
         fitness_scores = [self._fitness_function(individual, id_preds, target_ids_pos, masked_lm_logits) for individual in population]
         best_individual = min(zip(population, fitness_scores), key=lambda x: x[1])
         print(f"Best individual phrase: {best_individual[0]} with fitness: {best_individual[1]}")  # Debug output
-        bast_phrase_tensor = torch.zeros(len(best_individual[0]), dtype=torch.long)
-        for i in range(len(best_individual[0])):
-            bast_phrase_tensor[i]=best_individual[0][i]
-        bast_phrase_tokens=self._lm_tokenizer.convert_ids_to_tokens(bast_phrase_tensor)
-        bast_phrase=" ".join(token.replace("##", "") for token in bast_phrase_tokens)
-        return bast_phrase
+        
+        phrase = self._lm_tokenizer.convert_ids_to_tokens(best_individual[0])
+        return ' '.join(phrase)
 
     def _ga_replacement(self, current_text, start_idx, end_idx, id_preds, masked_lm_logits):
         """使用遗传算法获取替换单词或短语。
