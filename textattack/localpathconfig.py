9fd36f39013c33a4f9ddfe776abc5d3fa4669781
张磊 <1044836856@qq.com>
2024-12-02 14:27:07 +0800

    windows和linux的文件系统不一样进行了修改

--- textattack/localpathconfig.py
+++ textattack/localpathconfig.py
@@ -4,16 +4,28 @@ import os
 class LocalPathConfig:
 
     current_dir = os.path.dirname(os.path.realpath(__file__))
+    ##############################################################WINDOWS###########################################################
     #bert-base-uncased
-    BERT_BASE_UNCASED =  os.path.abspath(os.path.join(current_dir, '../../google/bert-base-uncased'))
+    BERT_BASE_UNCASED = os.path.abspath(os.path.join(current_dir, '..\\..\\google\\bert-base-uncased'))
 
-    UNIVERSAL_SENTENCE_ENCODER = os.path.abspath(os.path.join(current_dir, '../../google/universal-sentence-encoder'))
+    UNIVERSAL_SENTENCE_ENCODER = os.path.abspath(
+        os.path.join(current_dir, '..\\..\\google\\universal-sentence-encoder'))
 
-    UNIVERSAL_SENTENCE_ENCODER_LARGE = os.path.abspath(os.path.join(current_dir, '../../google/universal-sentence-encoder-large'))
-    
-    WORDENBEDDINGS=os.path.abspath(os.path.join(current_dir, '../../word_embeddings'))
-    
-    
+    UNIVERSAL_SENTENCE_ENCODER_LARGE = os.path.abspath(
+        os.path.join(current_dir, '..\\..\\google\\universal-sentence-encoder-large'))
+
+    WORDENBEDDINGS = os.path.abspath(os.path.join(current_dir, '..\\..\\word_embeddings'))
+
+    #############################################################LINUX##############################################################
+    # bert-base-uncased
+    BERT_BASE_UNCASED_L = os.path.abspath(os.path.join(current_dir, '../../google/bert-base-uncased'))
+
+    UNIVERSAL_SENTENCE_ENCODER_L = os.path.abspath(os.path.join(current_dir, '../../google/universal-sentence-encoder'))
+
+    UNIVERSAL_SENTENCE_ENCODER_LARGE_l = os.path.abspath(
+        os.path.join(current_dir, '../../google/universal-sentence-encoder-large'))
+
+    WORDENBEDDINGS_L = os.path.abspath(os.path.join(current_dir, '../../word_embeddings'))
     
     
 if __name__ == "__main__":
